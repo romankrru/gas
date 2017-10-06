379b52ffc2800025dc8ca14b25e8ee8cdf7011b7
Acamaz Tedeti <tedeev.a@domovenok.su>
2017-10-06 18:59:32 +0300

    Update configures

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
@@ -7,22 +8,62 @@ const rigger = require('gulp-rigger');
 const stylus = require('gulp-stylus');
 const uglify = require('gulp-uglify');
 const watch = require('gulp-watch');
+const webpack = require('gulp-webpack');
+const through = require('through2');
+// const webpackConfig = require('./webpack.config.js');
 
 const path = {
   in: './templates',
   out: './dist'
 };
 
-gulp.task('build:html', () => {});
-gulp.task('build:css', () => {
+function buildHMTL() {
+  return gulp.src(`${path.in}/**/*.html`)
+    .pipe(rigger())
+    .pipe(gulp.dest(path.out))
+    .pipe(browserSync.stream());
+}
+
+function buildCSS() {
   return gulp.src(`${path.in}/**/*.styl`)
     .pipe(stylus({ compress: true }))
-    .pipe(gulp.dest(`${path.out}/css`))
-});
-gulp.task('build:js', () => {});
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(path.out))
+    .pipe(browserSync.stream());
+}
+
+function buildJS() {
+  var fileName;
+  return gulp.src(`${path.in}/**/*.js`)
+    .pipe(through.obj(function (chunk, enc, cb) {
+      let fPath = chunk.path;
+      let extPos = fPath.lastIndexOf('.');
+      fileName = fPath.slice(fPath.lastIndexOf('\\') + 1, extPos);
+      console.log(fileName);
+      cb(null, chunk)
+    }))
+    .pipe(webpack({
+      output: {
+        fileName: '[name].bundle.js'
+      }
+    }))
+    .pipe(babel({ presets: ["es2015"] }))
+    .pipe(uglify())
+    .pipe(gulp.dest(path.out))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('build:html', buildHMTL);
+
+gulp.task('build:css', buildCSS);
+
+gulp.task('build:js', buildJS);
+
 gulp.task('serve', ['build:html', 'build:css', 'build:js'], () => {
-  watch(`${path}/html`, () => {
-    
-  });
+  browserSync.init({ server: path.out });
+  watch(`${path.in}/**/*.html`, buildHMTL);
+  watch(`${path.in}/**/*.styl`, buildCSS);
+  watch(`${path.in}/**/*.js`, buildJS);
 });
+
 gulp.task('default', ['serve']);
