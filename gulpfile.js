ba56d60043e08e80ffeaafd986840535a94e7fb5
Acamaz Tedeti <atsatedeev@gmail.com>
2017-10-07 01:52:06 +0300

    Configure image optimization, add new blocks

--- gulpfile.js
+++ gulpfile.js
@@ -2,68 +2,69 @@ const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
+const cleanCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const imageMin = require('gulp-imagemin');
 const rename = require('gulp-rename');
 const rigger = require('gulp-rigger');
 const stylus = require('gulp-stylus');
 const uglify = require('gulp-uglify');
 const watch = require('gulp-watch');
 const webpack = require('gulp-webpack');
-const through = require('through2');
-// const webpackConfig = require('./webpack.config.js');
 
 const path = {
-  in: './templates',
-  out: './dist'
+  html: {
+    src: './templates/*.html',
+    dist: './public',
+    watch: './templates/**/*.html'
+  },
+  style: {
+    src: './media/style/*.styl',
+    dist: './public/css',
+    watch: ['./common.blocks/**/*.styl', './media/style/*.styl']
+  },
+  image: {
+    src: ['./media/image/**/*.jpg', './media/image/**/*.png'],
+    dist: './public/img'
+  }
 };
 
 function buildHMTL() {
-  return gulp.src(`${path.in}/**/*.html`)
+  return gulp.src(path.html.src)
     .pipe(rigger())
-    .pipe(gulp.dest(path.out))
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest(path.html.dist))
     .pipe(browserSync.stream());
 }
 
 function buildCSS() {
-  return gulp.src(`${path.in}/**/*.styl`)
-    .pipe(stylus({ compress: true }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(path.out))
+  return gulp.src(path.style.src)
+    .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(cleanCss())
+    .pipe(rename({
+      dirname: '',
+      suffix: '.min',
+    }))
+    .pipe(gulp.dest(path.style.dist))
     .pipe(browserSync.stream());
 }
 
-function buildJS() {
-  var fileName;
-  return gulp.src(`${path.in}/**/*.js`)
-    .pipe(through.obj(function (chunk, enc, cb) {
-      let fPath = chunk.path;
-      let extPos = fPath.lastIndexOf('.');
-      fileName = fPath.slice(fPath.lastIndexOf('\\') + 1, extPos);
-      console.log(fileName);
-      cb(null, chunk)
-    }))
-    .pipe(webpack({
-      output: {
-        fileName: '[name].bundle.js'
-      }
-    }))
-    .pipe(babel({ presets: ["es2015"] }))
-    .pipe(uglify())
-    .pipe(gulp.dest(path.out))
-    .pipe(browserSync.stream());
+function optimizeImages() {
+  return gulp.src(path.image.src)
+    .pipe(imageMin())
+    .pipe(rename({ dirname: '' }))
+    .pipe(gulp.dest(path.image.dist));
 }
 
 gulp.task('build:html', buildHMTL);
-
 gulp.task('build:css', buildCSS);
+gulp.task('build:img', optimizeImages);
 
-gulp.task('build:js', buildJS);
-
-gulp.task('serve', ['build:html', 'build:css', 'build:js'], () => {
-  browserSync.init({ server: path.out });
-  watch(`${path.in}/**/*.html`, buildHMTL);
-  watch(`${path.in}/**/*.styl`, buildCSS);
-  watch(`${path.in}/**/*.js`, buildJS);
+gulp.task('serve', ['build:html', 'build:css'], () => {
+  browserSync.init({ server: path.html.dist });
+  watch(path.html.watch, buildHMTL);
+  watch(path.style.watch, buildCSS);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['build:img', 'serve']);
