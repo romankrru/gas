6c6dad5e30080accb59a9ce0bbf3503ea568ca8a
Acamaz Tedeti <atsatedeev@gmail.com>
2017-10-08 10:25:42 +0300

    Add logo

--- common.blocks/menu/menu.js
+++ common.blocks/menu/menu.js
@@ -1,7 +1,11 @@
+import $ from jquery;
+
 const Menu = {
   init() {
-
+    this.$el = $('.menu');
+    this.$closeButton = this.$el.find('.menu__closer');
   }
+  close() {}
 };
 
 export default Menu;
